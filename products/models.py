98d82f8620c13b8c8f8c7f38c9b0213b5e709076
Mubariz00534 <mubarizmirzayev77@gmail.com>
2020-03-31 17:06:16 +0400

    Signal Function added and some templates customized

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import random
 import os
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator
 
 # plt.jpg
 
@@ -56,3 +58,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# save() signalini tutmaq ucundur.
+pre_save.connect(product_pre_save_receiver, sender=Product)
